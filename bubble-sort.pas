42bfdea172efbb32771a4ee719dae102fc95862b
Štěpán Pešout <stepan@pesout.eu>
2018-07-26 17:50:04 +0200

    Translation

--- bubble-sort.pas
+++ bubble-sort.pas
@@ -1,73 +1,59 @@
-program Razeni; {Will be translted soon.}
+program bubble_sort;
 uses crt;
+var a, b, c, d, e : integer;
+var numbers: array[1..99999999] of integer;
 
-var
-  a, b, c, d, e : integer;
-  var pole: array[1..99999999] of integer;
 
-{---------------------------------------------}
-
-Procedure vstup;
-
+Procedure input;
 begin
+   write('Numbers: ');
    b := 1;
    for b:=1 to a do
      begin
-       write('Zadej ',b,'. cislo: ');
-       read(pole[b]);
+       read(numbers[b]);
      end;
 end;
 
-{---------------------------------------------}
 
-Procedure razeni_jednotlive;
-
+Procedure sorting_individual;
 begin
    b := 1;
    for b:=1 to a-1 do
      begin
-       if (pole[b] > pole[b+1]) then begin
-                                        c := pole[b];
-                                        pole[b] := pole[b+1];
-                                        pole[b+1] := c;
-                                     end;
+       if (numbers[b] > numbers[b+1]) then
+           begin
+              c := numbers[b];
+              numbers[b] := numbers[b+1];
+              numbers[b+1] := c;
+           end;
      end;
 end;
 
-{---------------------------------------------}
 
-Procedure razeni;
-
+Procedure sorting;
 begin
-   for d:=1 to a do razeni_jednotlive;
+   for d:=1 to a do sorting_individual;
 end;
 
-{---------------------------------------------}
 
-Procedure vystup;
-
+Procedure output;
 begin
    writeln;writeln;
    write(' |');
-
    e := 1;
    for e:=1 to a do
      begin
-       write(' ',pole[e],' |');
+       write(' ',numbers[e],' |');
      end;
 end;
 
-{---------------------------------------------}
 
 begin
-   writeln('Razeni podle velikosti od nejmensiho.');
-   write('Pocet cisel, ktera chcete seradit: ');
+   write('How many numbers: ');
    read(a);
    writeln;
-
-   vstup;
-   razeni;
-   vystup;
-
+   input;
+   sorting;
+   output;
    readln;readln;
 end.
